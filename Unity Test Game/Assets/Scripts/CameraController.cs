18f9fa0cf44c1b79ccbb37e313ba3fdc299d4abf
Luís Reis <luiscubal@gmail.com>
2016-03-02 22:53:02 +0000

    Basic camera

---
+++ Unity Test Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Player;
+    public float CameraRotationSpeed = 3;
+
+    Vector3 offset;
+    
+	void Start()
+    {
+        UpdateOffset();
+    }
+
+    void UpdateOffset()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        var playerPosition = Player.transform.position;
+
+        var horizontalCameraAxis = Input.GetAxis("Camera Horizontal") * CameraRotationSpeed;
+
+        var rotationCenter = playerPosition + new Vector3(0, offset.y, 0);
+
+        transform.RotateAround(rotationCenter, Vector3.up, horizontalCameraAxis);
+
+        UpdateOffset();
+    }
+        
+	void LateUpdate()
+    {
+        var playerPosition = Player.transform.position;
+
+        transform.position = playerPosition + offset;
+	}
+}

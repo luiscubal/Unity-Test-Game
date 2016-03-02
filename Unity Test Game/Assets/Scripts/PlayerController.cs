1b8b5e6477740b2a8afc045220b2e07558dbc15a
Luís Reis <luiscubal@gmail.com>
2016-03-02 00:42:21 +0000

    Roll ball forward
    Also added a few new axis.

---
+++ Unity Test Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 3;
+    private Rigidbody body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var moveVertical = Input.GetAxis("Vertical") * Speed;
+
+        body.AddForce(Camera.main.transform.forward * moveVertical);
+    }
+}

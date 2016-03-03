e65095d4ca2e770f66dab4aaafa8b89b225fd6fc
Luís Reis <luiscubal@gmail.com>
2016-03-03 23:12:18 +0000

    Show Score and Victory UI

--- Unity Test Game/Assets/Scripts/PlayerController.cs
+++ Unity Test Game/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 3;
+    public Text ScoreText;
+    public Text WinText;
+
     private Rigidbody body;
+    private int score;
 
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        score = 0;
+
+        UpdateDisplay();
     }
 
     void FixedUpdate()
@@ -20,5 +27,26 @@ public class PlayerController : MonoBehaviour
         var forward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;
 
         body.AddForce(forward * moveVertical + right * moveHorizontal);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+
+            ++score;
+            UpdateDisplay();
+        }
+    }
+
+    void UpdateDisplay()
+    {
+        ScoreText.text = "Score: " + score;
+
+        if (score >= 3)
+        {
+            WinText.gameObject.SetActive(true);
+        }
     }
 }

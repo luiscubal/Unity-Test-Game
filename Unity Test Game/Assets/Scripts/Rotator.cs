b595618d35df0be96a39e2164e8c8aee62e78def
Luís Reis <luiscubal@gmail.com>
2016-03-03 22:42:36 +0000

    Add basic pickup objects

---
+++ Unity Test Game/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+    public float RotationSpeed = 1;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * RotationSpeed * Time.deltaTime);
+    }
+}
